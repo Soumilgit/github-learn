07f03140320455894a5744c9398f9e6130b97357
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-04-15 23:33:26 +0530

    Create FootballTraining.cpp

---
+++ Codechef-Starters/FootballTraining.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int X,Y;
+	cin>>X>>Y;
+	if(X>Y){
+	    cout<<"FREEKICK"<<endl;
+	}
+	else{
+	    cout<<"PENALTY"<<endl;
+	}
+	return 0;
+
+}
