44d075880c3c9f10f0632817e3b7152253e6f835
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-04-16 01:03:53 +0530

    Update AC Please.cpp added problem statement as comment.

--- Codechef-Starters/AC Please.cpp
+++ Codechef-Starters/AC Please.cpp
@@ -1,3 +1,15 @@
+/*
+AC Please
+Chef is feeling the heat this summer and wants to switch on the Air Conditioning all the time, but since he doesn't want to harm the environment much, he only switches it on when the room temperature is strictly greater than 30 degrees celsius.
+Given the room temperature, you need to find whether Chef will switch on the air conditioning or not.
+
+Input Format
+The only line of input will contain a single integer T - denoting the room temperature.
+Output Format
+Print YES if Chef will turn the Air Conditioning on, otherwise print NO.
+
+You may print each character of the string in uppercase or lowercase (for example, the strings YES, yEs, yes, and yeS will all be treated as identical).
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
