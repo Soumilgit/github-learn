b432c1557335c90a7ed109221e3145bc9f6aa9f2
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-04-16 01:09:38 +0530

    Update MoodyChef.cpp added problem statement as comment.

--- Codechef-Starters/MoodyChef.cpp
+++ Codechef-Starters/MoodyChef.cpp
@@ -1,3 +1,21 @@
+/*
+Moody Chef
+Chef loves integers that are in the range of l to r. More formally, Chef loves an integer x if it satisfies the condition l≤x≤r.
+Chef has an array A of length N. Currently, the happiness of Chef is 0.
+He will examine the elements of the array in order from index 1 to N.
+
+If he finds an integer that he loves, his happiness will increase by 1; otherwise, his happiness will decrease by 1.
+Find the values of maximum and minimum happiness Chef will experience while going through the array.
+
+Input Format
+The first line of input will contain a single integer T, denoting the number of test cases.
+Each test case consists of multiple lines of input.
+The first line of each test case contains three integers N, l and r — the number of elements in the array, the least number that chef loves, the biggest number that chef loves.
+The next contains N space-separated integers, where the ℎi thinteger denotes A i.
+
+Output Format
+For each test case, output on a new line, two space-separated integers denoting the maximum and minimum happiness Chef will experience.
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
