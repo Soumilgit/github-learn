a5e9fef0c971f2642cca1034ae8dfca89e83f70c
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-04-15 23:42:48 +0530

    Create HealthySleep.cpp

---
+++ Codechef-Starters/HealthySleep.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int H;
+    cin>>H;
+    if(H < 8){
+        cout<<"LESS"<<endl;
+    }
+    else if (H == 8){
+        cout<<"PERFECT"<<endl;
+    }
+    else{
+        cout<<"MORE"<<endl;
+    }
+    return 0;
+    
+    
+	// your code goes here
+
+}
