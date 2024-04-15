f9b5d1b0ea7a9e1fb1630da43e2edc57684ab036
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-04-15 23:45:39 +0530

    Create Leg Space.cpp

---
+++ Codechef-Starters/Leg Space.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N,M;
+    cin>>N>>M;
+    if(N<M){
+        cout<<"YES"<<endl;
+    }
+    if(N==M){
+        cout<<"NO"<<endl;
+                
+        
+    }
+            
+                
+            
+        
+        
+    
+    
+   
+    
+        
+            
+        
+    
+    return 0;
+    
+	// your code goes here
+
+}
