77075b008823066b328e70818b269020a5c77c32
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-04-15 23:38:33 +0530

    Create The Ides Of March.cpp

---
+++ Codechef-Starters/The Ides Of March.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int N;
+	cin>>N;
+	if(N==15){
+	    cout<<"Yes"<<endl;
+	}
+	else{
+	    cout<<"No"<<endl;
+	}
+	return 0;
+
+}
