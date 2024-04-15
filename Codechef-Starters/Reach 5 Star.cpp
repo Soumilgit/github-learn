fa3be23adfd9c3cdca1baf4f7197c513b1e9f77a
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-04-16 01:06:32 +0530

    Update Reach 5 Star.cpp added problem statement as comment.

--- Codechef-Starters/Reach 5 Star.cpp
+++ Codechef-Starters/Reach 5 Star.cpp
@@ -1,3 +1,18 @@
+/*
+Reach 5 Star
+Chef loves giving contests on Codechef. Chef wants to become 5 star rated. Currently his rating on Codechef is X.
+After today's contest, his rating will increase by Y. Note that Y can be negative which means that Chef's rating will decrease.
+Find whether Chef will become 5 star rated after today's contest.
+Chef will be considered 5 star rated if his rating is greater than or equal to 2000.
+Input Format
+The first line contains two space-separated integers X and Y — chef's initial rating and the amount by which his rating will increase, respectively.
+Output Format
+Output YES if chef will become 
+5
+5 star rated, and NO otherwise.
+
+You may print each character of the string in uppercase or lowercase (for example, the strings YES, yEs, yes, and yeS will all be treated as identical).
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
