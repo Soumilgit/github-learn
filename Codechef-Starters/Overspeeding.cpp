a63a957d889a622c39347e089ce3a1d38ba5b73d
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-04-15 23:40:24 +0530

    Create Overspeeding.cpp

---
+++ Codechef-Starters/Overspeeding.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int S;
+    cin>>S;
+    if(S>40){
+        cout<<"YES"<<endl;
+        
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+	// your code goes here
+
+}
