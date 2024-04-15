51e449aaebb8d25ce53bceb3a400b6ad05ac642a
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-04-15 23:39:14 +0530

    Create AC Please.cpp

---
+++ Codechef-Starters/AC Please.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	if(T>30){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+
+}
