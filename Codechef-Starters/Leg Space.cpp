759f10fe7f3b528f1430ce7150701d04605a5136
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-04-16 00:58:59 +0530

    Update Leg Space.cpp added problem statement as comment.

--- Codechef-Starters/Leg Space.cpp
+++ Codechef-Starters/Leg Space.cpp
@@ -1,3 +1,21 @@
+/*
+	Leg Space
+There are N students including Chef in a school. The school bus has 
+M seats, and every student in the school travels on the bus.
+It is guaranteed that N≤M, so that everyone will have a seat.
+
+Chef is happy when the school bus is not full.
+Given N and M, your task is to find out whether Chef will be happy.
+
+Input Format
+The only line of input will contain two space-separated integers 
+N and M, denoting the number of students in the school and the number of seats in the school bus respectively.
+
+Output Format
+For each test case, output on a new line the answer: YES, if Chef will be happy, and NO otherwise.
+
+You may print each character of the string in uppercase or lowercase (for example, the strings YeS, yEs, yes, and YES will all be treated as identical).
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
