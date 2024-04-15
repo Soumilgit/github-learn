d74122f075de8d01b459b969841de1fa8a5bfb0b
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-04-16 01:02:32 +0530

    Update 50-50 Rule.cpp added problem statement as comment.

--- Codechef-Starters/50-50 Rule.cpp
+++ Codechef-Starters/50-50 Rule.cpp
@@ -1,3 +1,17 @@
+/*
+50-50 Rule
+Schools of Chefland use the 50−50 rule to grade their students. As per the rule , students are awarded :Z grade, if their attendance is strictly less than 
+50% .F grade, if they score strictly less than 50%.
+50% marks, given that their attendance is greater than or equal to 50% .
+A grade, otherwise.
+You are given two integers, X and Y, denoting the percentage of attendance and percentage of marks obtained by Alice. What grade will Alice get?
+Input Format
+The first line of input will contain a single integer T, denoting the number of test cases.
+Each test case consists of two space-separated integers X and Y — the percentage of attendance and percentage of marks obtained by Alice.
+Output Format
+For each test case, output on a new line, the grade awarded to Alice.
+Note that you may print the grade in lowercase or uppercase.
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
