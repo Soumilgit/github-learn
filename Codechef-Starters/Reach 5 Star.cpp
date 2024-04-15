1a702f51274d7827ea7b8d5fbcdd55fe2b5dcaf9
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-04-15 23:35:44 +0530

    Create Reach 5 Star.cpp

---
+++ Codechef-Starters/Reach 5 Star.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int X,Y;
+	cin>>X>>Y;
+	if(X+Y>=2000){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+
+}
