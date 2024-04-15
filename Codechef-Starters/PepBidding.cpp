454b4afb729b7a03b812ba0ef1d7bbc3234b45d7
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-04-16 01:16:05 +0530

    Update PepBidding.cpp added problem statement as comment.

--- Codechef-Starters/PepBidding.cpp
+++ Codechef-Starters/PepBidding.cpp
@@ -1,3 +1,120 @@
+/*
+Pep Bidding
+Pep is known for his golden bids on football matches. Surprisingly, he has a rather simple strategy!
+
+Team A and Team P are playing against each other, and each of them have N players.
+For each i from 1 to N,Pep believes that the i-th player on team A has an attack power of attA i, and a defense power of defA i.
+Similarly, the i-th player on team P has an attack power of attP i, and a defense power of defP i.
+The total attack power of a team is the sum of the attack powers of all of its 
+N players.
+The total defense power is defined similarly.
+
+If the total attack power of one team is strictly greater than the total attack power of the other team, and its total defense power is strictly greater than the total defense power of the other team, Pep bids for that team.
+Otherwise, he'll bid for a draw.
+
+Can you tell what Pep will bid on?
+
+Input Format
+The first line of input will contain a single integer T, denoting the number of test cases.
+Each test case consists of five lines of input.
+The first line of each test case contains a single integer N, the number of players on both teams.
+The second line contains N space-separated integers 
+1
+,
+2
+,
+…
+,
+
+attA 
+1
+​
+ ,attA 
+2
+​
+ ,…,attA 
+N
+​
+  — the elements of 
+
+attA.
+The third line contains 
+
+N space-separated integers 
+
+1
+,
+
+2
+,
+…
+,
+
+defA 
+1
+​
+ ,defA 
+2
+​
+ ,…,defA 
+N
+​
+  — the elements of 
+
+defA.
+The fourth line contains 
+
+N space-separated integers 
+
+1
+,
+
+2
+,
+…
+,
+
+attP 
+1
+​
+ ,attP 
+2
+​
+ ,…,attP 
+N
+​
+  — the elements of 
+
+attP.
+The fifth line contains 
+
+N space-separated integers 
+
+1
+,
+
+2
+,
+…
+,
+
+defP 
+1
+​
+ ,defP 
+2
+​
+ ,…,defP 
+N
+​
+  — the elements of 
+
+defP.
+Output Format
+For each test case, output on a new line a string representing what Pep bids for: either "A", "P", or "DRAW" (without quotes).
+
+Each letter of the output may be printed in either lowercase or uppercase, i.e, the strings DRAW, draw, dRaW, and so on will be considered equivalent.
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
